36b6469edc1b9ca719a057183752e607b3208466
1000copy <1000copy@gmail.com>
2014-11-29 22:35:46 +0800

    -

---
+++ golang_test.go
@@ -0,0 +1,14 @@
+package golang
+
+import (
+//  invalid import path: "testing "
+//  "testing "
+"testing"
+)
+// expected declaration, found '{'
+
+func Test_pass(t *testing.T){
+	t.Log("pass")
+	// t.Error("就是这么有钱，就是这么任性！")
+}
+/
