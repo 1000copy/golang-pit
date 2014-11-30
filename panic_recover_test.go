5efa9e438366f2d3f8420a0c96eaec19c34145bb
1000copy <1000copy@gmail.com>
2014-11-30 10:01:58 +0800

    -

--- panic_recover_test.go
+++ panic_recover_test.go
@@ -5,6 +5,7 @@ import (
 "fmt"
 "reflect"
 )
+
 func Test_pointer_runtime_reflect(test *testing.T){
 	// defer recover
 	defer func(){
@@ -15,27 +16,12 @@ func Test_pointer_runtime_reflect(test *testing.T){
 			   (v.String() != "invalid memory address or nil pointer dereference") {
 				test.Error("errorString expected")
 			}
-			
 		}
 	}()
 	var a *int
 	*a  = 1 // panic 
 }
-func Test_pointer_malloc(test *testing.T){
-	// declare var like this :
-	var a *int
-	a = new (int)
-	*a  = 1 // panic silent
-	if *a != 1 {
-		test.Error("1 expected")
-	}
-	// or like this:
-	b:= new(int)
-	*b = 2
-	if *b != 2 {
-		test.Error("2 expected")	
-	}
-}
+
 func inplace_ref_fmt(){
 	fmt.Print()
 }
--- pointer_test.go
+++ pointer_test.go
@@ -27,3 +27,19 @@ func Test_pointer(t *testing.T){
 		t.Error("inc expected result",*a)
 	}
 }
+
+func Test_pointer_malloc(test *testing.T){
+	// declare var like this :
+	var a *int
+	a = new (int)
+	*a  = 1 // panic silent
+	if *a != 1 {
+		test.Error("1 expected")
+	}
+	// or like this:
+	b:= new(int)
+	*b = 2
+	if *b != 2 {
+		test.Error("2 expected")	
+	}
+}
